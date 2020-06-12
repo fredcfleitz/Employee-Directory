dd335ee6871c44bda9bb9d41667f891220fd65d4
Frederick Fleitz <fredcfleitz@comcast.net>
2020-06-12 01:21:49 -0400

    dockerized frontend

--- crud/src/main/java/com/crud/controller/EmployeeController.java
+++ crud/src/main/java/com/crud/controller/EmployeeController.java
@@ -26,7 +26,7 @@ import com.crud.model.Employee;
 import com.crud.repository.EmployeeRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
